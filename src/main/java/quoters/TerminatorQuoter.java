e9d5da7ca9e836e2fc5cce89e9fd28346da3b19d
sysloparich <sysloparich@gmail.com>
2017-12-01 00:37:27 +0300

    added init method

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -1,5 +1,7 @@
 package quoters;
 
+import javax.annotation.PostConstruct;
+
 public class TerminatorQuoter implements Quoter{
 
     public String getMessage() {
@@ -14,6 +16,16 @@ public class TerminatorQuoter implements Quoter{
 
     @InjectRandomInt(min = 2, max = 7)
     private int repeat;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Phase 2");
+        System.out.println(repeat);
+    }
+
+    public TerminatorQuoter() {
+        System.out.println("Phase 1");
+    }
 
     public void sayQuote() {
         for (int i = 0; i < repeat; i++) {
